7ca93f29ad932888ea30c7b5ac82bb819d41c8cf
Mahmoud Garwallane <garwallanemahmoud@gmail.com>
2022-07-13 00:56:38 +0100

    feat: added tabs widgets and logout functionality to settings button

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -3,23 +3,72 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:continual_care/app/app.dart';
 import 'package:continual_care/home/home.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   static Page page() => const MaterialPage<void>(child: HomePage());
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int groupIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final user = context.select((AppBloc bloc) => bloc.state.user);
     return Scaffold(
+      backgroundColor: Color(0xff2A3066),
       appBar: AppBar(
-        title: const Text('Home'),
+        toolbarHeight: 60,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 18, top: 16, bottom: 16),
+          child: const Text(
+            'Gene Yllanes',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
         actions: <Widget>[
-          IconButton(
-            key: const Key('homePage_logout_iconButton'),
-            icon: const Icon(Icons.exit_to_app),
-            onPressed: () => context.read<AppBloc>().add(AppLogoutRequested()),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 18,
+            ),
+            child: IconButton(
+                key: const Key('homePage_logout_iconButton'),
+                icon: Image.asset(
+                  "assets/settings_icon.png",
+                  width: 35,
+                  height: 35,
+                ),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            title: Text("Logout ?"),
+                            content: Text("Do you accept?"),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text("No")),
+                              TextButton(
+                                  onPressed: () {
+                                    context
+                                        .read<AppBloc>()
+                                        .add(AppLogoutRequested());
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text("Yes"))
+                            ],
+                          ));
+                }),
           )
         ],
       ),
@@ -27,13 +76,101 @@ class HomePage extends StatelessWidget {
         alignment: const Alignment(0, -1 / 3),
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Avatar(photo: user.photo),
-            const SizedBox(height: 4),
-            Text(user.email ?? '', style: textTheme.headline6),
-            const SizedBox(height: 4),
-            Text(user.name ?? '', style: textTheme.headline5),
-          ],
+          children: <Widget>[],
+        ),
+      ),
+      bottomNavigationBar: Row(
+        children: [
+          BottomNavigationBarItem(
+            groupIndex: groupIndex,
+            title: "Home",
+            iconPath: "assets/home_icon.png",
+            index: 0,
+            onSelected: (index) {
+              setState(() {
+                groupIndex = index;
+              });
+              print(groupIndex);
+            },
+          ),
+          BottomNavigationBarItem(
+            groupIndex: groupIndex,
+            title: "Messages",
+            iconPath: "assets/messages_icon.png",
+            index: 1,
+            onSelected: (index) {
+              setState(() {
+                groupIndex = index;
+              });
+              print("here is the group index : " + groupIndex.toString());
+            },
+          ),
+          BottomNavigationBarItem(
+            titleColor: Colors.white,
+            activeColor: Color.fromARGB(143, 208, 4, 4),
+            inactiveColor: Color(0xffD00404),
+            groupIndex: groupIndex,
+            title: "Alert",
+            iconPath: "assets/warning_icon.png",
+            index: 2,
+            onSelected: (index) {
+              setState(() {
+                groupIndex = index;
+              });
+              print("here is the group index : " + groupIndex.toString());
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavigationBarItem extends StatelessWidget {
+  final String title;
+  final String iconPath;
+  final int index;
+  final Color activeColor;
+  final Color inactiveColor;
+  final Color titleColor;
+  int groupIndex;
+  final Function(int index) onSelected;
+  BottomNavigationBarItem({
+    this.titleColor = const Color(0xff2A3066),
+    this.activeColor = const Color(0xff7895A1),
+    this.inactiveColor = const Color(0xffCCCCCC),
+    required this.groupIndex,
+    required this.index,
+    required this.onSelected,
+    required this.iconPath,
+    required this.title,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: groupIndex == index ? activeColor : inactiveColor,
+        height: 64,
+        child: InkWell(
+          onTap: () {
+            onSelected(index);
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                iconPath,
+                width: 35,
+                height: 35,
+              ),
+              Text(
+                title,
+                style: TextStyle(color: titleColor),
+              ),
+            ],
+          ),
         ),
       ),
     );
