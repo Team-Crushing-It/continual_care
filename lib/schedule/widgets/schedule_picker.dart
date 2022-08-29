8f59f861ffe292306c5f9061a1ac3d36dc8eb2e3
Gene <yllanesgene@gmail.com>
2022-08-29 14:21:33 -0400

    chore: Added scroll controller

--- lib/schedule/widgets/schedule_picker.dart
+++ lib/schedule/widgets/schedule_picker.dart
@@ -5,7 +5,9 @@ import '../bloc/schedule_bloc.dart';
 import '../models/models.dart';
 
 class SchedulePicker extends StatefulWidget {
-  const SchedulePicker({Key? key,}) : super(key: key);
+  const SchedulePicker({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<SchedulePicker> createState() => _SchedulePickerState();
@@ -14,25 +16,47 @@ class SchedulePicker extends StatefulWidget {
 class _SchedulePickerState extends State<SchedulePicker> {
   late List<dynamic> weeks;
   late List<bool> tilesState;
+  late ScrollController _controller;
+
   @override
   void initState() {
     weeks = WeeksGenerator(
-            startTime: DateTime.now().subtract(Duration(days: 182)), endTime: DateTime.now().add(Duration(days: 182)))
+            startTime: DateTime.now().subtract(Duration(days: 182)),
+            endTime: DateTime.now().add(Duration(days: 182)))
         .generate();
     tilesState = List.generate(weeks.length, (index) => false);
-    super.initState();
+
+    _controller = ScrollController(initialScrollOffset: 1056);
+
+//  WidgetsBinding.instance
+//         .addPostFrameCallback((_) => animateToTile(1000));
+//     super.initState();
+  }
+
+  animateToTile(double pozish) {
+    print('onScrolld $pozish');
+    _controller.animateTo(pozish,
+        duration: Duration(seconds: 1), curve: Curves.ease);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 40,
       padding: EdgeInsets.all(8),
       color: Color(0xffF6E7E7),
       child: ListView.builder(
+        controller: _controller,
         itemExtent: 33,
         itemCount: weeks.length,
         itemBuilder: ((context, index) {
+
+          
           if (weeks[index] is List) {
             tilesState.add(false);
             WeekListTile tile = WeekListTile(
@@ -42,6 +66,7 @@ class _SchedulePickerState extends State<SchedulePicker> {
                   " - " +
                   weeks[index][1].day.toString(),
               onPressed: () {
+                animateToTile(index*33);
                 context.read<ScheduleBloc>().add(
                     ScheduleFilterChanged(weeks[index][0], weeks[index][1]));
 
