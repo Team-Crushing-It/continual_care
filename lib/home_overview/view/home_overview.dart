94c806b4bdae1d0836a9f03a1dd6f66b32a9c73e
Mahmoud Garwallane <garwallanemahmoud@gmail.com>
2022-08-31 18:52:24 +0100

    feat: build upcoming job logic

--- lib/home_overview/view/home_overview.dart
+++ lib/home_overview/view/home_overview.dart
@@ -1,6 +1,10 @@
+import 'package:continual_care/home_overview/cubit/home_overview_cubit.dart';
 import 'package:continual_care/home_overview/home_overview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jobs_repository/jobs_repository.dart';
 
+import '../../schedule/widgets/job_list_tile.dart';
 
 class HomeOverviewPage extends StatelessWidget {
   const HomeOverviewPage({Key? key}) : super(key: key);
@@ -12,10 +16,34 @@ class HomeOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (context) =>
+            HomeOverviewCubit(repository: context.read<JobsRepository>())
+              ..getUpcoming(),
+        child: HomeOverviewView());
+  }
+}
+
+class HomeOverviewView extends StatelessWidget {
+  const HomeOverviewView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Home Overview'),
         ),
-        body: Text('homeoverview'));
+        body: BlocBuilder<HomeOverviewCubit, HomeOverviewState>(
+          builder: (context, state) {
+            return Column(
+              children: [
+                if (state.job != null)
+                  JobListTile(job: state.job!)
+                else
+                  Text("NO upcomming jobs")
+              ],
+            );
+          },
+        ));
   }
 }
